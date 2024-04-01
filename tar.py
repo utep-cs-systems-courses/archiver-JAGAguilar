59266884aa34d4d33b0e3fd69091c063005e979b
Jose Antonio Aguilar <joseaguilarbusiness@gmail.com>
2024-03-31 23:19:40 -0600

    almost done with inband

--- tar.py
+++ tar.py
@@ -2,76 +2,134 @@
 
 import os
 import sys
-import struct
 from buf import BufferedFdReader, BufferedFdWriter
 
 def createArch(archName,fileList):
-    arch_fd = os.open(archName, os.O_WRONLY | os.O_CREAT)
+    #open the defined archive file
+    arch_fd = os.open(archName, os.O_WRONLY| os.O_CREAT)
     writer = BufferedFdWriter(arch_fd)
-    for fName in fileList:
-        fBytes = fName.encode()
-        writer.writeByte(len(fBytes))
-        for byte in fBytes:
-            writer.writeByte(byte)
-        fSize = os.fstat(fBytes)
-        for byte in struct.pack("!Q", fSize):
-            writer.writeByte(byte)
-        curFile = os.open(fName,os.O_RDONLY)
+
+    #Archive all files listed
+    for file in fileList:
+        curFile = os.open(file, os.O_RDONLY)
         reader = BufferedFdReader(curFile)
-        bufferedCopy(reader,writer)
-        os.close(curFile)
+        file_size = os.path.getsize(file)
+
+        #Archive file begins with fileheader
+        fileHeader = bytearray(64)
+        for i in range(len(file)):
+            fileHeader[i] = file[i].encode()[0]#encode the file name
+        for i in range(len(str(file_size))):
+            fileHeader[i+32] = str(file_size)[i].encode()[0]# then 32 bits later encode the file_size
+
+        #store file contents outside of fileheader
+        bt = reader.readByte()
+        content = []
+        while bt is not None:
+            content.append(bt)
+            bt=reader.readByte()
+
+        #combine fileheader and contents to be written
+        toWrite = fileHeader +  bytearray(content)
+        for i in toWrite:
+            writer.writeByte(i)
         reader.close()
     writer.close()
-    os.close(arch_fd)
-        
-'''
-def extractArch(archName):
-    output_dir = os.path.splitext(archName)[0]
-    os.makedirs(output_dir,exist_ok=True)
 
-    arch = os.open(archName,os.O_RDONLY)
-    reader = BufferedFdReader(arch)
-    while True:
-        nameByte = reader.readByte()
-        if nameByte is None:
-            break
-        nameSize = int.from_bytes(nameByte, byteorder='big')
-        fileNameBytes = b''
-        for byte in nameSize:
-            fileNameBytes += reader.readByte(byte)
-        if fileNameBytes is None:
-            break
-        fileName = fileNameBytes.decode()
-        fileSize = fstat()
-    
-    
-'''
+
 def extractArch(archName):
-    readableArch = os.open(archName,os.O_RDONLY)
-    read = os.read(readableArch,1).decode()
-    while read != "":
-        fNameSize = ""
-        fSize = ""
-        while read != "":
-            fNameSize = fNameSize + read
-            read = os.read(readableArch, 1).decode()
-        fName = os.read(inputFile, int(fNameSize)).decode()
-        outFile = os.open(fName, os.O_WRONLY | os.O_CREAT)
-        read = os.read(readableArch, 1).decode()
-        while read != "":
-            fSize = fSize + read
-            read = os.read(readableArch, 1).decode()
-        os.write(outFile, os.read(readableArch, int(fSize)))
-        read = os.read(readableArch, 1).decode()
-        os.close(outFile)
-    os.close(readableArch)
-'''
+    # Open the archive file for reading
+    arch_fd = os.open(archName, os.O_RDONLY)
+    reader = BufferedFdReader(arch_fd)
+    
+    # Loop to extract files
+    while True:
+        # Read file header
+        fileHeader = bytearray(64)
+        for i in range(len(fileHeader)):
+            byte = reader.readByte()
+            if byte is None:
+                break
+            fileHeader[i] = byte
+        
+        # Break if there are no more files
+        if not any(fileHeader):
+            break
+        
+        # Extract filename and file size from the header
+        fileName = fileHeader[:32].decode().strip('\x00')
+        fileSize = int(fileHeader[32:64].decode().strip('\x00'))
+        
+        # Read file content
+        fileContent = bytearray()
+        for _ in range(fileSize):
+            byte = reader.readByte()
+            if byte is None:
+                break
+            fileContent.append(byte)
+        
+        # Write file to its respective path
+        curFile = os.open(fileName, os.O_WRONLY | os.O_CREAT)
+        writer = BufferedFdWriter(curFile)
+        for i in fileContent:
+            writer.writeByte(i)
+        writer.close()
+    # Close the archive file
+    reader.close()
+
+    
 def ibArchive(archName, archList):
     arch_fd = os.open(archName, os.O_WRONLY | os.O_CREAT)
     writer = BufferedFdWriter(arch_fd)
+    terminator = b':::'
+
+    
     for fName in archList:
-   '''     
-        
+        curFile = os.open(fName, os.O_RDONLY)
+        fileHeader = bytearray(32)
+        for i in range(len(fName)):
+            fileHeader[i+12] = fName[i].encode()[0]
+
+            
+        content = []
+        reader = BufferedFdReader(curFile)
+        bt = reader.readByte()
+        while bt is not None:
+            content.append(bt)
+            bt = reader.readByte()
+        toWrite = fileHeader + bytearray(content) + terminator
+        for i in toWrite:
+            writer.writeByte(i)
+        reader.close()
+    writer.close()
+
+def ibExtract(archName):
+    arch_fd = os.open(archName, os.O_RDONLY)
+    reader = BufferedFdReader(arch_fd)
+    terminator = b':::'
+
+    while True:
+        fileHeader = bytearray(32)
+        for i in range(len(fileHeader)):
+            bt = reader.readByte()
+            if bt is None:
+                break
+            fileHeader[i] = bt
+        fileName = fileHeader.decode().strip('\x00')
+        if not fileName:
+            break
+        curFile = os.open(fileName, os.O_WRONLY | os.O_CREAT)
+        writer = BufferedFdWriter(curFile)
+        while True:
+            bt = reader.readByte()
+            if bt is None or bt == terminator:
+                break
+            writer.writeByte(bt)
+        if bt == terminator:
+            os.close(curFile)
+            continue
+        writer.close()
+    reader.close()
     
 
     
@@ -89,5 +147,14 @@ if __name__ == "__main__":
     elif cmd == 'x':
         archive = sys.argv[2]
         extractArch(archive)
+        os.write(1,"Done!\n".encode())
+    elif cmd == 'ic':
+        archive = sys.argv[2]
+        archList = sys.argv[3:]
+        ibArchive(archive,archList)
+        os.write(1,"Done!\n".encode())
+    elif cmd == 'ix':
+        archive = sys.argv[2]
+        ibExtract(archive)
         os.write(1,"Done!\n".encode())
         
